959a32c3cedc0ccb001ca202bef025cce7c6ab99
Niborwood <48519867+Niborwood@users.noreply.github.com>
2022-02-16 11:54:31 +0100

    Added warning text

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import Battle from './containers/Battle';
-import Menu from './containers/Menu';
+import Battle from "./containers/Battle";
+import Menu from "./containers/Menu";
 
-import './App.scss';
+import "./App.scss";
 
 function App({ isBattling }) {
   return (
     <div className="app">
-      <div className={isBattling ? 'app__pokemonBattle app__shake' : 'app__pokemonBattle'}>
+      <div
+        className={
+          isBattling ? "app__pokemonBattle app__shake" : "app__pokemonBattle"
+        }
+      >
         <Battle />
         <div className="menu">
           <Menu />
         </div>
+      </div>
+      <div className="app__warning">
+        <p>
+          Cette (toute) petite application s&apos;arrête simplement à choisir
+          une attaque, lancer l&apos;animation et que le Sablaireau d&apos;en
+          face riposte.
+        </p>
+        <p>
+          C&apos;est tout, mais pour apprendre Redux, c&apos;est déjà pas mal !
+        </p>
       </div>
     </div>
   );
